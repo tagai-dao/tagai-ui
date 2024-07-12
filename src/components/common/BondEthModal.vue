479104602973427cbda694b0c2e6cf14778d6364
wangxi <wangxi8206@sina.com>
2024-07-12 18:23:19 +0800

    update bond eth address

--- src/components/common/BondEthModal.vue
+++ src/components/common/BondEthModal.vue
@@ -1,35 +1,27 @@
 <script setup lang="ts">
-import { getProviders, setActiveProviderDetail } from "@/utils/wallets";
+import { EthWalletState, useAccountStore } from "@/stores/web3";
 import { computed, ref } from "vue";
+import ChoseWallet from '@/components/common/ChoseWallet.vue'
+import { getProvider, getProviderInfo } from "@/utils/wallets";
 
+const accStore = useAccountStore();
 const wallet = ref();
 const loading = ref(false);
-const providers = computed(() => {
-  return getProviders() ?? [];
-});
+const step = ref('selectAddress')
 
-async function onSelectWalletMeta(wallet: any) {
-  setActiveProviderDetail(wallet);
-  wallet.value = wallet;
-}
+const provider = computed(() => {
+    return getProvider();
+})
+
 </script>
 
 <template>
-  <div>
-    BondEth
-    <button
-      class="w-full border-1 gradient-border shadow-shadow12 px-20px h-48px rounded-full flex justify-center items-center gap-10px hover:border-color85 mt-12px"
-      v-for="wallet of providers"
-      :key="wallet.info.uuid"
-      :disabled="loading"
-      @click="onSelectWalletMeta(wallet)"
-    >
-      <img class="w-20px h-20px" :src="wallet.info.icon" alt="" />
-      <div
-        class="block min-w-100px text-center flex justify-center items-center gap-4px text-gradient-primary"
-      >
-        <span class="font-bold">{{ wallet.info.name }}</span>
-      </div>
-    </button>
+  <div v-if="accStore.ethConnectState !== EthWalletState.Connected">
+    <ChoseWallet/>
+  </div>
+  <div v-else>
+        <div v-if="step === 'selectAddress'">
+            {{ accStore.ethConnectAddress }}
+        </div>
   </div>
 </template>
