ee6e53f3cb97f9fff436759927114d76f6e3770f
TTT <somiar2010@hotmail.com>
2024-07-15 13:15:43 +0800

    update img

--- src/composables/useUploadImg.ts
+++ src/composables/useUploadImg.ts
@@ -8,8 +8,31 @@ export const useUploadImg = () => {
     uploadType.value = type
     const reader = new FileReader()
     reader.readAsDataURL(options.file)
-    reader.onload = (res) => {
-    }
+    // 可上传的文件
+    // const img = options.file
+    // let param = new FormData();
+    // param.append("file", img);
+    // if (img.size > 2048000) {
+    //   reject(errCode.LARGE_IMG)
+    //   return;
+    // }
+    // const config = {
+    //   headers: {
+    //     "Content-Type": "multipart/form-data",
+    //   },
+    // };
+    // axios
+    //   .post(QN_UPLOAD_URL, param, config)
+    //   .then((res) => {
+    //     resolve(res?.data?.url);
+    //   })
+    //   .catch((err) => {
+    //     if (err.toJSON().message.indexOf('Request failed with status code 429') !== -1) {
+    //       reject(errCode.OUT_OF_USAGE)
+    //       return;
+    //     }
+    //     reject(errCode.UPLOAD_FAIL);
+    //   });
   }
 
   return {
