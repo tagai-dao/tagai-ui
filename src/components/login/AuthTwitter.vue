651c2cb5777b7d0171e29e014b34156672d2e64e
TTT <somiar2010@hotmail.com>
2024-07-19 12:52:09 +0800

    update tabbar

--- src/components/login/AuthTwitter.vue
+++ src/components/login/AuthTwitter.vue
@@ -85,7 +85,7 @@ async function login() {
   <div class="flex flex-col justify-center min-h-[240px] py-6 px-4">
     <div class="flex flex-col gap-y-[48px]">
       <div class="flex items-center gap-8 justify-center">
-        <img class="w-12 min-w-12 object-center object-contain" src="~@/assets/logo-primary.svg" alt="">
+        <img class="w-12 min-w-12 object-center object-contain" src="~@/assets/logo.svg" alt="">
         <img class="w-10 min-w-10 object-center object-contain" src="~@/assets/icons/icon-login-arrow.svg" alt="">
         <img class="w-8 min-w-8 object-center object-contain" src="~@/assets/icons/icon-x.svg" alt="">
       </div>
--- src/layout/TabBar.vue
+++ src/layout/TabBar.vue
@@ -22,25 +22,33 @@ async function createCoin() {
 </script>
 
 <template>
-  <div class="h-24 w-full bg-white flex justify-between items-center px-8 py-4">
-    <router-link to="/" class="flex flex-col items-center cursor-pointer gap-0.5 px-2">
-      <template v-if="$route.name==='home'">
-        <img class="w-6 h-6" src="~@/assets/icons/icon-tabbar-home-active.svg" alt="">
-        <span class="text-gradient bg-gradient-primary text-h5">Tag</span>
-      </template>
-      <template v-else>
-        <img class="w-6 h-6" src="~@/assets/icons/icon-tabbar-home.svg" alt="">
-        <span class="text-h5 text-grey-normal">Tag</span>
-      </template>
-    </router-link>
-    <button class="mb-6" @click="createCoin">
-      <img src="~@/assets/icons/icon-tabbar-create.svg" alt="">
-    </button>
-    <router-link to="/profile/1" class="flex flex-col items-center cursor-pointer gap-0.5 px-2">
-      <img v-if="accStore.getAccountInfo" class="w-6 h-6" :src="accStore.getAccountInfo.profile.replace('normal', '200x200')" alt="">
-      <img v-else class="w-6 h-6" src="~@/assets/icons/icon-tabbar-profile.svg" alt="">
-      <span class="text-h5 text-grey-normal">Profile</span>
-    </router-link>
+  <div class="relative">
+    <div class="absolute h-[76px] left-0 right-0 bottom-0 flex items-center justify-center rounded-md overflow-hidden">
+      <div class="flex-1 h-full bg-gradient-tab"></div>
+      <img class="h-full object-cover object-center" src="~@/assets/images/tabbar-bg.svg" alt="">
+      <div class="flex-1 h-full bg-gradient-tab"></div>
+    </div>
+    <div class="w-full flex justify-between items-end px-8 py-4 relative z-10">
+      <router-link to="/" class="w-16 flex flex-col items-center cursor-pointer gap-0.5 px-2">
+        <template v-if="$route.name==='home'">
+          <img class="w-6 h-6" src="~@/assets/icons/icon-tabbar-home-active.svg" alt="">
+          <span class="text-gradient bg-gradient-primary text-h5">Tag</span>
+        </template>
+        <template v-else>
+          <img class="w-6 h-6" src="~@/assets/icons/icon-tabbar-home.svg" alt="">
+          <span class="text-h5 text-grey-normal">Tag</span>
+        </template>
+      </router-link>
+      <button class="mb-9" @click="createCoin">
+        <img src="~@/assets/icons/icon-tabbar-create.svg" alt="">
+      </button>
+      <router-link to="/profile/1" class="w-16 flex flex-col items-center cursor-pointer gap-0.5 px-2">
+        <img v-if="accStore.getAccountInfo" class="w-6 h-6 min-w-6 min-h-6 rounded-full"
+             :src="accStore.getAccountInfo.profile.replace('normal', '200x200')" alt="">
+        <img v-else class="w-6 h-6" src="~@/assets/icons/icon-tabbar-profile.svg" alt="">
+        <span class="text-h5 text-grey-normal">Profile</span>
+      </router-link>
+    </div>
   </div>
 </template>
 
