38f594d7ea497ed48282cc9dffd9e29f298d2718
TTT <somiar2010@hotmail.com>
2024-07-20 15:06:46 +0800

    fix quote modal

--- src/components/tweets/TweetBtnQuote.vue
+++ src/components/tweets/TweetBtnQuote.vue
@@ -8,6 +8,7 @@ import {useCreateTweet} from "@/composables/useCreateTweet";
 import {useTweet} from "@/composables/useTweet";
 import { type Tweet } from "@/types";
 import { EmojiPicker } from 'vue3-twemoji-picker-final'
+import emptyAvatar from "@/assets/icons/icon-default-avatar.svg";
 
 const props = defineProps<{
     tweet: Tweet;
@@ -27,7 +28,8 @@ const {
   getBlur,
   onPaste,
   selectEmoji,
-  contentInput
+  contentInput,
+  showClear
 } = useCreateTweet()
 
 const headerProfileImg = () => {
@@ -62,102 +64,86 @@ const userQuote = () => {
           :class="tweet.quoted ? 'text-red-ff' : 'text-grey-bd'">
               {{ tweet.quoteCount ?? 0 }}</span>
   </button>
-  <el-dialog v-model="quoteVisible" width="700" align-center title="" destroy-on-close>
-    <div class="bg-color1C px-5 min-h-40vh sm:min-h-[300px] sm:pt-40px max-h-80vh relative">
-      <div class="modal-close-line text-center text-center">
-        <button class="w-50px h-6px rounded-full bg-colorA6"
-                @click="quoteVisible = false"></button>
-      </div>
-      <i-ep-close class="modal-close-icon text-colorA6 w-24px h-24px absolute right-20px top-20px"
-                  @click="quoteVisible = false"/>
-      <div class="flex-1 overflow-hidden py-20px flex">
-        <img class="mr-10px md:mr-1rem rounded-full gradient-border w-40px h-40px"
+  <el-dialog v-model="quoteVisible"
+             modal-class="overlay-white"
+             class="max-w-[500px] rounded-[20px]"
+             width="90%" :show-close="false" align-center destroy-on-close>
+    <div class="flex-1 overflow-hidden py-5 flex flex-col gap-2">
+      <div class="flex items-center gap-2">
+        <img class="w-8 h-8 rounded-full"
              :src="headerProfileImg()" alt=""/>
-        <div class="flex-1 overflow-x-hidden flex flex-col">
-          <div class="flex flex-col relative max-h-30vh overflow-auto">
-            <div
-                contenteditable
-                class="z-1 flex-1 pt-5px whitespace-pre-line leading-24px 2xl:leading-18px content-input-box break-word"
-                ref="contentRef"
-                @blur="getBlur"
-                @paste="onPaste"
-                @input="contentInput"
-                v-html="formatEmojiText(contentRef)"
-            ></div>
+        <div class="text-h3 text-black">Username</div>
+      </div>
+      <div class="bg-grey-f0/90 rounded-2xl px-4 pb-4">
+        <div class="max-h-[176px] overflow-hidden relative flex flex-col bg-grey-f0/90">
+          <div contenteditable
+               class="outline-none flex-1 overflow-auto no-scroll-bar min-h-[56px] pt-2 whitespace-pre-line
+                      text-lg z-10 relative"
+               ref="contentRef"
+               @input="contentInput"
+               @blur="getBlur"
+               @paste="onPaste"
+               v-html="contentEl"></div>
+          <div v-if="!showClear" class="absolute top-3 left-0 text-14px leading-24px z-0 opacity-30">
+            Please input
           </div>
-          <div class="mt-1rem border-1 border-color2A bg-color1C rounded-12px p-15px">
-            <div class="flex items-center flex-wrap">
-              <img class="rounded-full gradient-border w-1.6rem h-1.6rem mr-5px min-w-1.6rem"
-                   :src="profileImg" alt=""/>
-              <div class="flex items-center flex-wrap">
-                  <span class="c-text-black text-left mr-3 cursor-pointer text-14px leading-18px">
-                    {{ tweet.twitterName }}
-                  </span>
+        </div>
+        <div class="my-3 bg-white border-[1px] border-grey-light rounded-xl p-3">
+          <div class="flex items-center gap-2">
+            <img v-if="profileImg" class="w-8 h-8 min-w-8 min-h-8 rounded-full"
+                 :src="profileImg" alt=""/>
+            <img v-else class="w-8 h-8 min-w-8 min-h-8 rounded-full"
+                 src="~@/assets/icons/icon-default-avatar.svg" alt="">
+            <div class="flex-1">
+              <div class="text-base font-semibold leading-4">
+                {{ tweet.twitterName??'Username' }}
               </div>
-              <div class="flex items-center id-time">
-                  <span class="text-sm leading-18px text-grey-bd">
-                    @{{ tweet.twitterUsername }}
-                  </span>
-                <span class="mx-4px text-grey-bd"> · </span>
-                <span class="whitespace-nowrap">
-                  {{ parseTimestamp(tweet.tweetTime) }}
+              <div class="flex items-center gap-1 text-grey-light-active leading-3">
+                <span class="text-sm">
+                  @{{ tweet.twitterUsername??'username' }}
                 </span>
-              </div>
-            </div>
-            <div class="text-left font-400 mt-0.5rem">
-              <div class="text-sm leading-18px 2xl:text-14px 2xl:leading-20px text-colorD9 light:text-color46">
-                  <span v-if="isIgnoreAccount" class="text-blue-500 break-all">{{steemUrl}}</span>
-                <div v-else class="whitespace-pre-line break-word"
-                     v-html="formatEmojiText(content)"></div>
-              </div>
-              <div v-if="imgurls" class="text-colorD9 light:text-color46">
-                  <span v-for="(url, index) of imgurls.slice(0, 4)"
-                        :key="index" :title="url">[Pic]</span>
+                <span class=""> · </span>
+                <span class="whitespace-nowrap">
+                {{ parseTimestamp(tweet.tweetTime)??'**' }}
+              </span>
               </div>
             </div>
           </div>
-          <div class="flex justify-between py-1rem">
-            <div class="flex justify-between items-center">
-              <el-popover
-                  ref="emojiPopover"
-                  placement="top"
-                  trigger="click"
-                  width="300"
-                  :teleported="true"
-                  :persistent="false"
-              >
-                <template #reference>
-                  <img
-                      class="w-1.8rem h-1.8rem lg:w-1.4rem lg:h-1.4rem"
-                      src="~@/assets/icons/icon-emoji.svg"
-                      alt=""
-                  />
-                </template>
-                <template #default>
-                  <div class="h-310px lg:h-400px">
-                    <EmojiPicker
-                        :options="{
-                          imgSrc: '/emoji/',
-                          locals: $i18n.locale === 'zh' ? 'zh_CN' : 'en',
-                          hasSkinTones: false,
-                          hasGroupIcons: false,
-                        }"
-                        @select="(e: any) => selectEmoji(e)"
-                    />
-                  </div>
-                </template>
-              </el-popover>
+          <div class="text-left font-normal mt-2">
+            <div class="text-sm leading-5">
+              <span v-if="isIgnoreAccount" class="text-blue-500 break-all">{{steemUrl}}</span>
+              <div v-else class="whitespace-pre-line break-word multi-content multi-content-2"
+                   v-html="formatEmojiText(content)"></div>
             </div>
-            <button class="h-12 px-3 bg-gradient-primary text-white font-bold rounded-full text-lg
-                           flex items-center justify-center gap-2 disabled:opacity-30"
-                    :disabled="isQuoting"
-                    @click="userQuote">
+            <div v-if="imgurls" class="">
+              <span v-for="(url, index) of imgurls.slice(0, 4)" :key="index" :title="url">[Pic]</span>
+            </div>
+          </div>
+        </div>
+        <div class="flex justify-between ">
+          <div class="flex justify-between items-center">
+            <el-popover ref="emojiPopover" trigger="click" width="300" :teleported="true" :persistent="false">
+              <template #reference>
+                <img class="w-10 h-10"
+                     src="~@/assets/icons/icon-emoji.svg" alt=""/>
+              </template>
+              <template #default>
+                <div class="h-[310px] lg:h-[400px]">
+                  <EmojiPicker :options="{ imgSrc: '/emoji/', locals: 'en',hasSkinTones: false, hasGroupIcons: false}"
+                               @select="(e: any) => selectEmoji(e)"/>
+                </div>
+              </template>
+            </el-popover>
+          </div>
+          <button class="h-10 px-5 bg-gradient-primary text-white font-bold rounded-full text-lg
+                         flex items-center justify-center gap-2 disabled:opacity-30"
+                  :disabled="isQuoting"
+                  @click="userQuote">
                 <span class="text-white text-h5">
                   {{isDefaultQuote ? $t("curation.tweet") : $t("postView.createNewCuration")}}
                 </span>
-              <i-ep-loading v-if="isQuoting" class="animate-spin w-18px h-18px text-white"/>
-            </button>
-          </div>
+            <i-ep-loading v-if="isQuoting" class="animate-spin w-18px h-18px text-white"/>
+          </button>
         </div>
       </div>
     </div>
