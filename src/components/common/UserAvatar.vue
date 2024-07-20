f72f1faf874505c7d5352cccbe0fa959dc8a41ce
TTT <somiar2010@hotmail.com>
2024-07-20 11:30:23 +0800

    fix avatar

--- src/components/common/UserAvatar.vue
+++ src/components/common/UserAvatar.vue
@@ -60,7 +60,7 @@ function replaceEmptyImg(e: any) {
           </div>
         </div>
         <div class="pl-10 mt-2 text-grey-normal">
-          <div class="flex gap-x-2 whitespace-nowrap" @click="onCopy(btcAddress)">
+          <div class="flex gap-x-2 whitespace-nowrap" @click="onCopy(ethAddr ?? '')">
             <span>ETH Address</span>
             <span class="text-gradient bg-gradient-primary">{{ formatAddress(ethAddr ?? '') }}</span>
           </div>
