8125feda6b28c5c0a26e338c0fddf2a97781e2ac
wangxi <wangxi8206@sina.com>
2024-09-02 11:58:24 +0800

    add tag color

--- src/composables/useTags.ts
+++ src/composables/useTags.ts
@@ -1,2 +1,2 @@
-export const tagBgColors = ['#B6EAD3', '#C1C3FF', '#FAEDAA']
-export const tagTextColors = ['#6D8C7F', '#747599', '#968E66']
+export const tagBgColors = ['#B6EAD3', '#C1C3FF', '#FAEDAA', '#FACCAA', '#FFC1C1', '#FFC3F2', '#BCBEFF', '#9ADBFF']
+export const tagTextColors = ['#6D8C7F', '#747599', '#968E66', '#968E66', '#747599', '#747599', '#747599', '#747599']
