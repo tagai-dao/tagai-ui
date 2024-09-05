879fa83625577de6718e9ab071f91ad9888cd6e8
TTT <somiar2010@hotmail.com>
2024-09-05 19:29:23 +0800

    update rule

---
+++ src/components/common/RuleModal.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+import {GlobalModalType} from "@/types";
+</script>
+
+<template>
+  <div class="py-2">
+    <div class="flex justify-between items-center">
+      <span class="text-h2 text-grey-normal-hover">Rule</span>
+      <img class="cursor-pointer"
+           @click="$emit('onClose')"
+           src="~@/assets/icons/icon-modal-close.svg" alt=""/>
+    </div>
+    <div class="text-h5 leading-6 mt-4 mb-2">
+      The way tiptag prevents Rugs is that the first 10% of $Coins buy are locked up for 3 days.
+      Each coin on tiptag is a <span class="text-green-34">fair-launch</span> with <span class="text-blue-32">no presale</span> and
+      <span class="text-orange-normal">no team allocation</span>.
+    </div>
+    <ul class="leading-6 flex flex-col gap-3">
+      <li>
+        step 1：buy the Tagcoin on the bonding curve
+      </li>
+      <li>
+        step 2：post or like tweet, host or participate in Twitter Space, get TagCoin
+      </li>
+      <li>
+        step 3：if someone trades, 1% transaction fee flows to the deployer
+      </li>
+      <li>
+        step 4：once someone transacts with a Blink on twitter, 1% of the transaction fee flows to the Blink publisher.
+      </li>
+      <li>
+        step 5：10 $ETH of liquidity is then deposited in uniswap and burned
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/home/IconLinks.vue
+++ src/components/home/IconLinks.vue
@@ -9,14 +9,14 @@ const props = defineProps<{
 
 <template>
   <div class="h-4 flex gap-1 pb-1">
-    <a v-if="community?.twitter" href="#" target="_blank">
-      <img class="w-3" src="~@/assets/icons/icon-link-x.svg" alt="">
+    <a v-if="!community?.twitter" href="#" target="_blank">
+      <img class="w-3 min-w-3" src="~@/assets/icons/icon-link-x.svg" alt="">
     </a>
-    <a v-if="community?.telegram" href="#" target="_blank">
-      <img class="w-3" src="~@/assets/icons/icon-link-tg.svg" alt="">
+    <a v-if="!community?.telegram" href="#" target="_blank">
+      <img class="w-3 min-w-3" src="~@/assets/icons/icon-link-tg.svg" alt="">
     </a>
-    <a v-if="community?.official" href="#" target="_blank">
-      <img class="w-3" src="~@/assets/icons/icon-link-official.svg" alt="">
+    <a v-if="!community?.official" href="#" target="_blank">
+      <img class="w-3 min-w-3" src="~@/assets/icons/icon-link-official.svg" alt="">
     </a>
   </div>
 </template>
