c7118c2d5e9a2f6ebb0269a9b4a2ee0fe976538b
terry3t <wangxi8206@sina.com>
2024-09-08 21:09:05 +0800

    update rules

--- src/components/common/RuleModal.vue
+++ src/components/common/RuleModal.vue
@@ -22,13 +22,13 @@
         2：70% of tokens are allocated via a bonding curve to raise initial liquidity.
       </li>
       <li>
-        3：Once 10 ETH is raised, the liquidity pool (10 ETH + 15% tokens) will be added to Uniswap, and LP tokens will be burned (liquidity locked).
+        3：Once 10 ETH is raised, the liquidity pool (30000 $NULS + 15% tokens) will be added to Uniswap, and LP tokens will be burned (liquidity locked).
       </li>
       <li>
-        step 4：once someone transacts with a Blink on twitter, 1% of the transaction fee flows to the Blink publisher.
+        Step 4：Once someone transacts with a Blink on twitter, 1% of the transaction fee flows to the Blink publisher.
       </li>
       <li>
-        step 5：30000 $NULS of liquidity is then deposited in pheasantswap and burned
+        Step 5：30000 $NULS of liquidity is then deposited in PheasantSwap and burned.
       </li>
     </ul>
   </div>
