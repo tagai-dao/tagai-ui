10e34112fcc5c9779faab01532ed9c141be51ed0
wangxi <wangxi8206@sina.com>
2024-09-23 16:27:42 +0800

    update rules

--- src/components/common/RuleModal.vue
+++ src/components/common/RuleModal.vue
@@ -12,24 +12,24 @@ import {GlobalModalType} from "@/types";
            src="~@/assets/icons/icon-modal-close.svg" alt=""/>
     </div>
     <div class="text-h5 leading-6 mt-4 mb-2">
-      Each coin on tiptag is a <span class="text-green-34">fair-launch</span> with <span class="text-blue-32">no presale</span> and
-      <span class="text-orange-normal">no team allocation</span>.
+      Fair launch: Every coin on TipTag has a <span class="text-green-34">fair start</span> with <span class="text-blue-32">no presale</span> or
+      <span class="text-orange-normal">team allocation</span>, giving everyone an equal opportunity.
     </div>
     <ul class="leading-6 flex flex-col gap-3">
       <li>
-        Step 1：Buy the Tagcoin on the bonding curve
+        Step 1：Buy coins using the bonding curve, where the price increases as more coins are bought.
       </li>
       <li>
-        Step 2：Post or like tweet, host or participate in Twitter Space, get TagCoin
+        Step 2：Earn coins by posting, liking tweets, hosting, or joining Twitter Spaces. Active engagement is rewarded.
       </li>
       <li>
-        Step 3：If someone trades, 1% transaction fee flows to the deployer
+        Step 3：When someone trades, 1% of the transaction fee goes to the person who launched the coin.
       </li>
       <li>
-        Step 4：Once someone transacts with a Blink on twitter, 1% of the transaction fee flows to the Blink publisher.
+        Step 4：If the trade involves a Blink (special X post), 1% of the fee goes to the Blink publisher.
       </li>
       <li>
-        Step 5：10 $ETH of liquidity is then deposited in Uniswap and burned
+        Step 5：10 $ETH and 15% of the coins are added to Uniswap for liquidity, and the LP tokens are burned to reduce supply and support the price.
       </li>
     </ul>
   </div>
