f954ef333f6e92619a9f55be3e3c7813e16423b6
terry3t <wangxi8206@sina.com>
2024-10-06 22:04:46 +0800

    update rule

--- src/components/common/RuleModal.vue
+++ src/components/common/RuleModal.vue
@@ -11,24 +11,21 @@
            src="~@/assets/icons/icon-modal-close.svg" alt=""/>
     </div>
     <div class="text-h5 leading-6 mt-4 mb-2">
-      Fair launch: Every coin on TipTag has a <span class="text-green-34">fair start</span> with <span class="text-blue-32">no presale</span> or
-      <span class="text-orange-normal">team allocation</span>, giving everyone an equal opportunity.
+      TipTag Launchpad: <span class="text-green-34">No presale</span>, <span class="text-blue-32">no team allocation - equal opportunity</span>
+      for all participants.
     </div>
     <ul class="leading-6 flex flex-col gap-3">
       <li>
-        Step 1：Buy coins using the bonding curve, where the price increases as more coins are bought.
+        1：15% of tokens are distributed for social rewards, incentivizing content creation and community engagement.
       </li>
       <li>
-        Step 2：Earn coins by posting, liking tweets, hosting, or joining Twitter Spaces. Active engagement is rewarded.
+        2：70% of tokens are allocated via a bonding curve to raise initial liquidity.
       </li>
       <li>
-        Step 3：When someone trades, 1% of the transaction fee goes to the person who launched the coin.
+        3：Once 10 ETH is raised, the liquidity pool (10 ETH + 15% tokens) will be added to Uniswap, and LP tokens will be burned (liquidity locked).
       </li>
       <li>
-        Step 4：If the trade involves a Blink (special X post), 1% of the fee goes to the Blink publisher.
-      </li>
-      <li>
-        Step 5：10 $ETH and 15% of the coins are added to Uniswap for liquidity, and the LP tokens are burned to reduce supply and support the price.
+        4：Coin Deployer and Blink creators will receive 1% of transaction fees from trades.
       </li>
     </ul>
   </div>
