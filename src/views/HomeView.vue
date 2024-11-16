8e23ee0c2a26206745d00f7acf652c69a9bacfe6
terry3t <wangxi8206@sina.com>
2024-11-16 20:05:54 +0800

    remove eth

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,7 +24,7 @@ const finished = ref(false)
 const { setInter } = useInterval()
 const { pageScroll, pageScrollTo} = usePageScroll()
 const pageScrollRef = ref()
-const tabOptions = ['Base', 'ETH', 'ENULS']
+const tabOptions = ['Base', 'ENULS']
 const activeTab = ref('Base')
 
 watch(listType, (val) => {
@@ -105,8 +105,6 @@ async function getSpaces() {
 function gotoChain(chain: string){
   if (chain === 'ENULS') {
     window.open('https://enuls.tiptag.social', '__blank')
-  }else if(chain === 'ETH') {
-    window.open('https://eth.tiptag.social', '__blank')
   }
 }
 
