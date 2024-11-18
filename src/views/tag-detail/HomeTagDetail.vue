a1f224ef2386ef90864b58abc82d7d866705d810
wangxi <wangxi8206@sina.com>
2024-11-18 17:55:38 +0800

    add icon

--- src/views/tag-detail/HomeTagDetail.vue
+++ src/views/tag-detail/HomeTagDetail.vue
@@ -188,6 +188,9 @@ onMounted(async () => {
           <div class="flex flex-wrap justify-between gap-x-4 items-center">
             <div class="flex items-center">
               <span class="text-black text-h2">{{ comStore.currentSelectedCommunity?.tick }}</span>
+              <button v-if="comStore.currentSelectedCommunity?.createdByAi" class="pl-2 h-5 text-sm rounded-md gradient-text glow-effect">
+                AI create with
+              </button>
               <IconLinks :community="comStore.currentSelectedCommunity"/>
             </div>
             <div class="text-base flex gap-1">
@@ -338,5 +341,52 @@ onMounted(async () => {
 </template>
 
 <style scoped>
+.gradient-text {
+  background: linear-gradient(
+    300deg,
+    #ff0080,
+    #ff8c00,
+    #40e0d0,
+    #7b68ee,
+    #ff0080
+  );
+  background-size: 300%;
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  animation: gradient 8s linear infinite;
+  font-weight: bold;
+}
 
+.glow-effect {
+  position: relative;
+}
+
+.glow-effect::before {
+  content: "AI create";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: inherit;
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  filter: blur(12px);
+  opacity: 0.7;
+  animation: gradient 8s linear infinite;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
 </style>
