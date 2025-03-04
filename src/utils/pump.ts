bb315b0abf27729e406bac2b16e14bcd1e203f1f
terry3t <wangxi8206@sina.com>
2025-03-05 06:01:16 +0800

    fix transaction

--- src/utils/pump.ts
+++ src/utils/pump.ts
@@ -76,7 +76,7 @@ export const sellToken = async (token: string, amount: bigint, receiveEth: bigin
     if (!ethers.isAddress(sellsman)) {
         sellsman = ethers.ZeroAddress;
     }
-    const tc = await getContract('Token', token)
+    const tc = await getContract('Token1', token)
     if (listed) {
         // checkout approve
         const allowance = await tc.allowance(useAccountStore().ethConnectAddress, uniswapV2Router02);
@@ -306,7 +306,6 @@ export const getTokenOnchainInfo = async (tokens: String[], versions: Record<str
                 }else {
                     result[key].price = res[key + '-1'] / res[key + '-2']
                 }
-                console.log(42, key, result[key].price)
             }else{
                 result[key].price = res[key + '-price']
             }
@@ -317,7 +316,7 @@ export const getTokenOnchainInfo = async (tokens: String[], versions: Record<str
 
 export const getBuyAmountWithETHAfterFee = async (token: string | undefined, version: number, amount: bigint) => {
     if (!token) return 0n
-    const tc = await getContract('Token', token, true);
+    const tc = await getContract('Token1', token, true);
     const supply = await tc.bondingCurveSupply();
     const pumpC = await getContract('Pump' + version, version == 1 ? PumpContract1 : PumpContract2, true);
     const receive = await pumpC.getBuyAmountByValue(supply, amount * 9800n / 10000n)
@@ -326,7 +325,7 @@ export const getBuyAmountWithETHAfterFee = async (token: string | undefined, ver
 
 export const getReceivedAmountSellETHAfterFee = async (token: string | undefined, version: number, amount: bigint) => {
     if (!token) return 0n
-    const tc = await getContract('Token', token, true);
+    const tc = await getContract('Token1', token, true);
     const pumpC = await getContract('Pump' + version, version == 1 ? PumpContract1 : PumpContract2, true);
     const supply = await tc.bondingCurveSupply();
     const receive = await pumpC.getSellPriceAfterFee(supply, amount)
