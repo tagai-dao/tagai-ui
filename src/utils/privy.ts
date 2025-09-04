23de0965633ff524de46584ad8cd4335e5845a3a
terry3t <wangxi8206@sina.com>
2025-09-04 21:54:54 +0800

    delete privy-js-core sdk

--- src/utils/privy.ts
+++ src/utils/privy.ts
@@ -1,5 +1,3 @@
-import Privy, { LocalStorage } from '@privy-io/js-sdk-core'
-import { PrivyConfig, ChainConfig } from '@/config'
 import { bsc } from 'viem/chains'
 
 export const customBsc = {
@@ -16,9 +14,9 @@ export const customBsc = {
   }
 }
 
-export const privy = new Privy({
-  appId: PrivyConfig.appId,
-  supportedChains: [customBsc],
-  storage: new LocalStorage(),
-  clientId: PrivyConfig.clientId,
-})
+// export const privy = new Privy({
+//   appId: PrivyConfig.appId,
+//   supportedChains: [customBsc],
+//   storage: new LocalStorage(),
+//   clientId: PrivyConfig.clientId,
+// })
