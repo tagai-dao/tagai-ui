1ea23074cec3054d25d48126a07b5761e8b0cd65
terry <terry3t@gmail.com>
2025-09-05 09:44:53 +0800

    fix privy login

--- src/react_app/AuthLoading.jsx
+++ src/react_app/AuthLoading.jsx
@@ -2,10 +2,13 @@ import {useLoginWithOAuth, useOAuthTokens, useWallets, useCreateWallet} from "@p
 import {privyLogin} from "../apis/api.ts";
 import emitter from "../utils/emitter.ts";
 import {useEffect} from "react";
+import { sleep } from "@/utils/helper";
+import { useAccountStore } from "@/stores/web3";
 
 export default function AuthLoading() {
     const { state, loading, initOAuth } = useLoginWithOAuth();
     const {wallets, ready} = useWallets()
+    const accStore = useAccountStore();
     const { createWallet } = useCreateWallet({
         onSuccess: (async ({wallet}) => {
             const provider = await wallet.getEthereumProvider()
@@ -19,6 +22,16 @@ export default function AuthLoading() {
     useEffect(() => {
         async function getWalletProvider() {
             if(ready) {
+                let count = 8
+                // 没有推特登录的时候不会加载privy钱包
+                while (count > 0) {
+                    const logged = accStore.getAccountInfo?.twitterId
+                    if (logged) {
+                        break;
+                    }
+                    count--;
+                    await sleep(1)
+                }
                 if (wallets.length === 0 || !wallets.find((wallet) => wallet.walletClientType === 'privy')) {
                    await createWallet()
                     return;
