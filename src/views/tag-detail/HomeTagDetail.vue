60d2c5f038d9c352a54139f2db3ce32f944a5976
terry3t <wangxi8206@sina.com>
2025-12-15 22:27:07 +0800

    update

--- src/views/tag-detail/HomeTagDetail.vue
+++ src/views/tag-detail/HomeTagDetail.vue
@@ -43,7 +43,7 @@ const tabOptions = computed(() => {
     return [
       // {label: 'Group', key: 'group'},
       {label: 'Square', key: 'content'},
-      // {label: 'Predict', key: 'predict'},
+      {label: 'Predict', key: 'predict'},
       // {label: 'Tipped', key: 'tipped'},
       {label: 'activity', key: 'activity'},
       // {label: 'Trades', key: 'trade'},
@@ -55,7 +55,7 @@ const tabOptions = computed(() => {
   return [
     // {label: 'Group', key: 'group'},
     {label: 'Square', key: 'content'},
-    // {label: 'Predict', key: 'predict'},
+    {label: 'Predict', key: 'predict'},
     // {label: 'Tipped', key: 'tipped'},
     {label: 'activity', key: 'activity'},
     {label: 'Trades', key: 'trade'},
