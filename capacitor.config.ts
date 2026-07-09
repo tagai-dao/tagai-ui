b9edb3d15cbd88c1a8ec6d9531881bf101069296
0xNought <yinguozi925107@gmail.com>
2026-07-09 16:21:26 +0800

    feat(app): add capacitor native shell

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'fun.tagai.app',
+  appName: 'TagAI',
+  webDir: 'dist',
+}
+
+export default config
